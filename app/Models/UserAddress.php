9fe49e6005c51ce836aebf7e8fa8a97d6a6391a4
shxwcj <1131920105@qq.com>
2019-05-07 18:44:32 +0800

    收货地址列表

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    protected $fillable = [ 'province','city','district','address','zip','contact_name','contact_phone','last_used_at',];
+
+    protected $dates = ['last_used_at']; //表示 last_used_at 字段是一个时间日期类型
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFullAddressAttribute()
+    {
+        return "{$this->province}{$this->city}{$this->district}{$this->address}";
+    }
+}
