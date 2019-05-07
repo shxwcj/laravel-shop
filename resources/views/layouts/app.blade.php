a207438150b8827608be6519669bd383dd97d758
shxwcj <1131920105@qq.com>
2019-05-07 14:39:04 +0800

    基础布局

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Laravel Shop') - Laravel 电商教程</title>
     <!-- 样式 -->
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <link href="{{mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app" class="{{ route_class() }}-page">
-        @include('layouts._header')
-        <div class="container">
-            @yield('content')
-        </div>
-        @include('layouts._footer')
+<div id="app" class="{{route_class()}}-page">
+    @include('layouts._header')
+    <div class="container">
+        @yield('content')
     </div>
-    <!-- 样式 -->
-
+    @include('layouts._footer')
+</div>
+<!-- JS 脚本 -->
+<script src="{{ mix('js/app.js') }}"></script>
+@yield('scriptsAfterJs')
 </body>
 </html>
