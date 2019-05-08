7b825d5c2397d85d1a3ea9a315a5c8eeceac041c
shxwcj <1131920105@qq.com>
2019-05-08 20:43:50 +0800

    新增收获地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserAddressRequest;
+use App\Models\UserAddress;
 use Illuminate\Http\Request;
 
 class UserAddressesController extends Controller
 {
+    /**
+     * @desc 收货地址列表
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(Request $request)
     {
         return view('user_addresses.index',['addresses' => $request->user()->addresses]);
+    }
+
+    /**
+     * @desc 新增和编辑收货地址
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('user_addresses.create_and_edit',['address'=>new UserAddress()]);
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only(array('province','city','district','address','zip','contact_name','contact_phone')));
+        return redirect()->route('user_addresses.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::get('/','PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
-    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index'); //收货地址列表
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');//新增和编辑收货地址列表
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');       //添加收获地址
 });
