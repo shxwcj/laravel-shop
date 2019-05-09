70e81ee2e3d060fd6ca519500e6746b48feaff0c
shxwcj <1131920105@qq.com>
2019-05-09 11:20:09 +0800

    修改和删除收货地址

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,15 @@ Route::get('/','PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
-    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index'); //收货地址列表
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');        //收货地址列表
+
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');//新增和编辑收货地址列表
+
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');       //添加收获地址
+
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit'); //修改收货地址列表
+
+    Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');  //修改收获地址
+
+    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');  //删除收获地址
 });
