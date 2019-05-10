96e61dfa7d29d917316e0443d46246d7a686f218
shxwcj <1131920105@qq.com>
2019-05-10 12:27:31 +0800

    管理后台 - 新增、编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -13,7 +13,7 @@ use function foo\func;
 
 class ProductsController extends Controller
 {
-    use HasResourceActions;
+    use HasResourceActions;     //控制器中的增store删destroy改update方法均在此
 
     /**
      * Index interface.
@@ -28,6 +28,33 @@ class ProductsController extends Controller
             ->description('description')
             ->body($this->grid());
     }
+
+    /**
+     * create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('创建商品')
+            ->body($this->form());
+    }
+
+    /**
+     * edit interface.
+     * @param $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id,Content $content)
+    {
+        return $content
+            ->header('编辑商品')
+            ->body($this->form()->edit($id));
+    }
+
 
     /**
      * Make a grid builder.
@@ -62,5 +89,31 @@ class ProductsController extends Controller
             });
         });
         return $grid;
+    }
+
+    public function form()
+    {
+        $form = new Form(new Product());
+        // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+        $form->text('title','商品名称')->rules('required');
+        // 创建一个选择图片的框
+        $form->image('image','封面图片')->rules('required|image');
+        // 创建一个富文本编辑器
+        $form->editor('description','商品描述')->rules('required');
+        // 创建一组单选框
+        $form->radio('on_sale','上架')->options(['1' => '是', '0'=> '否'])->default('0');
+
+        $form->hasMany('skus','SKU 列表',function (Form\NestedForm $form){
+            $form->text('title','SKU 名称')->rules('required');
+            $form->text('description','SKU 描述')->rules('required');
+            $form->text('price','单价')->rules('required|numeric|min:0.01');
+            $form->text('stock','剩余库存')->rules('required|integer|min:0');
+        });
+
+        //定义事件回调，当模型即将保存时会出发这个回调
+        $form->saving(function (Form $form){
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
+        return $form;
     }
 }
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,6 +11,15 @@ Route::group([
 ], function (Router $router) {
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');                 //用户管理
-    $router->get('products', 'ProductsController@index')->name('products.index');   //商品管理
+
+    $router->get('products', 'ProductsController@index');           //商品管理
+
+    $router->get('products/create', 'ProductsController@create');   //创建商品列表
+
+    $router->post('products', 'ProductsController@store');          //添加商品
+
+    $router->get('products/{id}/edit', 'ProductsController@edit');  //编辑商品列表
+
+    $router->put('products/{id}', 'ProductsController@update');     //修改商品
 
 });
