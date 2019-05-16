8fa2497bae709d4874721444437b8eaf211c9ee5
shxwcj <1131920105@qq.com>
2019-05-16 19:02:56 +0800

    完善支付后逻辑

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use Carbon\Carbon;
 use Endroid\QrCode\QrCode;
 use Illuminate\Http\Request;
+use App\Events\OrderPaid;
 
 class PaymentController extends Controller
 {
@@ -78,7 +79,7 @@ class PaymentController extends Controller
             'payment_method' => 'alipay', // 支付方式
             'payment_no'     => $data->trade_no, // 支付宝订单号
         ]);
-
+        $this->afterPaid($order);
         return app('alipay')->success();
     }
 
@@ -139,8 +140,17 @@ class PaymentController extends Controller
             'payment_method' => 'wechat',
             'payment_no'     => $data->transaction_id,
         ]);
-
+        $this->afterPaid($order);
         return app('wechat_pay')->success();
+    }
+
+    /**
+     * @desc 创建支付成功事件
+     * @param Order $order
+     */
+    public function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 
 }
