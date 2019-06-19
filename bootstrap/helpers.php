be6188401c8d99edcb534d17a762c69957f8ca81
shxwcj <1131920105@qq.com>
2019-06-19 15:55:56 +0800

    开发环境加入 Ngrok

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -8,3 +8,20 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+/**
+ * @desc 配置暂时的公关内网环境
+ * @param $routeName
+ * @param array $parameters
+ * @return string
+ */
+function ngrok_url($routeName, $parameters = [])
+{
+    // 开发环境，并且配置了 NGROK_URL
+    if(app()->environment('local') && $url = config('app.ngrok_url')) {
+        // route() 函数第三个参数代表是否绝对路径
+        return $url.route($routeName, $parameters, false);
+    }
+
+    return route($routeName, $parameters);
+}
--- config/app.php
+++ config/app.php
@@ -15,6 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'ngrok_url' => env('NGROK_URL'),
     /*
     |--------------------------------------------------------------------------
     | Application Environment
