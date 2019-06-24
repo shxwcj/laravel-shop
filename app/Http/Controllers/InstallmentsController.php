004f1d3e18fd1bc3646c9dc7704a7505a67ccb91
shxwcj <1131920105@qq.com>
2019-06-24 17:19:05 +0800

    前台分期付款详情页

--- app/Http/Controllers/InstallmentsController.php
+++ app/Http/Controllers/InstallmentsController.php
@@ -20,4 +20,21 @@ class InstallmentsController extends Controller
         return view('installments.index',['installments'=>$installments]);
     }
 
+    /**
+     * 分期详情页
+     * @param Installment $installment
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(Installment $installment)
+    {
+        $this->authorize('own',$installment);
+        // 取出当前分期付款的所有的还款计划，并按还款顺序排序
+        $items = $installment->items()->orderBy('sequence')->get();
+        return view('installments.show', [
+            'installment' => $installment,
+            'items'       => $items,
+            // 下一个未完成还款的还款计划
+            'nextItem'    => $items->where('paid_at', null)->first(),
+        ]);
+    }
 }
