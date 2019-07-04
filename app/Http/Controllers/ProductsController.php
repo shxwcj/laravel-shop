17c98c8af5fae779ec6dbce6db5fa226b60fc425
shxwcj <1131920105@qq.com>
2019-07-04 10:57:53 +0800

    使用 Elasticsearch 重构商品搜索功能

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,6 +8,7 @@ use App\Models\OrderItem;
 use App\Models\Product;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductsController extends Controller
 {
@@ -21,6 +22,8 @@ class ProductsController extends Controller
         //创建一个查询构造器
         $builder  = Product::query()->where('on_sale',true);
         //判断是否有提交search参数，如果有就赋值给$search变量
+
+ // -----------------没用es之前 --------------------开始-----------------------------
         //search 参数用来模糊搜索商品
         if ($search = $request->input('search','')){
             $like = '%'.$search.'%';
@@ -34,6 +37,7 @@ class ProductsController extends Controller
                     });
             });
         }
+
         // 如果有传入 category_id 字段，并且在数据库中有对应的类目
         if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
             // 如果这是一个父类目
@@ -48,6 +52,7 @@ class ProductsController extends Controller
                 $builder->where('category_id', $category->id);
             }
         }
+
         //是否有提交order参数，如果有就赋值给$order变量
         //order参数用来控制商品的排序规则
         if ($order = $request->input('order','')){
@@ -67,6 +72,98 @@ class ProductsController extends Controller
             'category'=>$category??null,
 //            'categoryTree'=>$categoryService->getCategoryTree(),// 将类目树传递给模板文件  此已封装方法
         ]);  //其中一种方式即可
+
+// -----------------没用es之前 --------------------结束-----------------------------
+
+// -----------------用es之后 --------------------开始-----------------------------
+//        iif ($search = $request->input('search', '')) {
+//            // 将搜索词根据空格拆分成数组，并过滤掉空项
+//            $keywords = array_filter(explode(' ', $search));
+//
+//            $params['body']['query']['bool']['must'] = [];
+//            // 遍历搜索词数组，分别添加到 must 查询中
+//            foreach ($keywords as $keyword) {
+//                $params['body']['query']['bool']['must'][] = [
+//                    'multi_match' => [
+//                        'query'  => $keyword,
+//                        'fields' => [
+//                            'title^2',
+//                            'long_title^2',
+//                            'category^2',
+//                            'description',
+//                            'skus_title',
+//                            'skus_description',
+//                            'properties_value',
+//                        ],
+//                    ],
+//                ];
+//            }
+//        }
+//        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+//            if ($category->is_directory) {
+//                // 如果是一个父类目，则使用 category_path 来筛选
+//                $params['body']['query']['bool']['filter'][] = [
+//                    'prefix' => ['category_path' => $category->path.$category->id.'-'],
+//                ];
+//            } else {
+//                // 否则直接通过 category_id 筛选
+//                $params['body']['query']['bool']['filter'][] = ['term' => ['category_id' => $category->id]];
+//            }
+//        }
+//        //实现 分页与排序 --------开始
+//        $page = $request->input('page',1);
+//        $perPage = 16;
+//        //构建查询
+//        $params = [
+//            'index' => 'products',
+//            'type'  => '_doc',
+//            'body'  => [
+//                'from'  => ($page - 1) * $perPage, // 通过当前页数与每页数量计算偏移值
+//                'size'  => $perPage,
+//                'query' => [
+//                    'bool' => [
+//                        'filter' => [
+//                            ['term' => ['on_sale' => true]],
+//                        ],
+//                    ],
+//                ],
+//            ],
+//        ];
+//        //是否提交order参数，如果有就赋值给$order变量
+//        // order 参数用来控制商品的排序规则
+//        if ($order = $request->input('order', '')) {
+//            // 是否是以 _asc 或者 _desc 结尾
+//            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+//                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+//                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+//                    // 根据传入的排序值来构造排序参数
+//                    $params['body']['sort'] = [[$m[1] => $m[2]]];
+//                }
+//            }
+//        }
+//        $result = app('es')->search($params);
+//        // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
+//        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+//        // 通过 whereIn 方法从数据库中读取商品数据
+//        $products = Product::query()
+//            ->whereIn('id', $productIds)
+//            // orderByRaw 可以让我们用原生的 SQL 来给查询结果排序
+//            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $productIds)))
+//            ->get();
+//        // 返回一个 LengthAwarePaginator 对象
+//        $pager = new LengthAwarePaginator($products, $result['hits']['total'], $perPage, $page, [
+//            'path' => route('products.index', false), // 手动构建分页的 url
+//        ]);
+//        return view('products.index', [
+//            'products' => $pager,
+//            'filters'  => [
+//                'search' => $search,
+//                'order'  => $order,
+//            ],
+//           'category' => $category ?? null,
+//        ]);
+
+// -----------------用es之后 --------------------结束-----------------------------
 
 //        return view('products.index',compact('products',$products));
 //        return view('products.index')->with('products',$products);
